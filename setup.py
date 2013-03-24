6b1f9c4ddcbd86d305f195c462654d9b8c709fd6
Ivan Yurchenko <ivanhoe12@gmail.com>
2013-03-24 18:43:52 +0400

    setup.py added

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""
+flask-mongo-sessions
+====================
+
+Server-side sessions for Flask with MongoDB.
+"""
+from setuptools import setup
+
+
+setup(
+    name='flask-mongo-sessions',
+    version='0.1',
+    url='https://github.com/ivanyu/flask-mongo-sessions',
+    license='MIT',
+    author='Ivan Yurchenko',
+    author_email='ivan0yurchenko@gmail.com',
+    description='Server-side sessions for Flask with MongoDB',
+    long_description=__doc__,
+    py_modules=['flask_mongo_sessions'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.8',
+        'Flask-PyMongo',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite='tests',
+)
