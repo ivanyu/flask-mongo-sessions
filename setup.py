5004338457363217bb6a1bc21403d81c4f37b676
Ivan Yurchenko <ivanhoe12@gmail.com>
2013-03-31 23:08:49 +0400

    setup.py updated. Version 0.2.

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ from setuptools import setup
 
 setup(
     name='flask-mongo-sessions',
-    version='0.1',
+    version='0.2',
     url='https://github.com/ivanyu/flask-mongo-sessions',
     license='MIT',
     author='Ivan Yurchenko',
     author_email='ivan0yurchenko@gmail.com',
+    maintainer='Ivan Yurchenko',
+    maintainer_email='ivan0yurchenko@gmail.com',
     description='Server-side sessions for Flask with MongoDB',
     long_description=__doc__,
     py_modules=['flask_mongo_sessions', 'flask_mongo_sessions.tests'],
@@ -33,5 +35,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
+    keywords="flask mongodb pymongo flask-pymongo "
+             "mongoengine flask-mongoengine",
     test_suite='flask_mongo_sessions.tests.suite',
 )
